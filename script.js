6ed44fb36eb309d162947256ef3d9bdc4657d305
Ashwin-Code26 <ashwin26u@gmail.com>
2025-06-11 11:46:00 +0530

    Added my project files

---
+++ script.js
@@ -0,0 +1,17 @@
+function CalculateBmi(){
+    let weightInput= prompt("Enter your Weight in Kg:");
+    let heightInput= prompt("Enter your Height in cm:");
+        
+    let weight = parseFloat(weightInput);
+    let heightCm = parseFloat(heightInput);
+
+    let height = heightCm / 100;
+
+    let bmi = weight / (height * height);
+
+    document.getElementById("result").innerHTML = `Your BMI is: ${bmi.toFixed(2)}`;
+}
+
+
+
+
